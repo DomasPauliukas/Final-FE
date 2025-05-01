c9a5f6f2eff11d7da75313c0cc99730277828a30
DomasPauliukas <Domas0319@gmail.com>
2025-05-01 21:05:47 +0300

    privateroute

--- src/App.tsx
+++ src/App.tsx
@@ -10,22 +10,30 @@ import MyFestivalsPage from './pages/my-festivals/MyFestivalsPage'
 import PageNavigator from './components/pagenavigator/PageNavigator'
 import ProfilePage from './pages/profile/ProfilePage'
 import EditProfile from './pages/profile/EditProfile'
+import PrivateRoute from './components/privateroute/PrivateRoute'
 
 function App() {
-
   return (
     <>
     <BrowserRouter>
       <PageNavigator />
       <Routes>
         <Route index element={<HomePage />}/>
+
         <Route path='/login' element={<LoginPage />}/>
         <Route path='/register' element={<RegisterPage />}/>
-        <Route path='/profile' element={<ProfilePage />}/>
-        <Route path='/profile-edit' element={<EditProfile />}/>
+
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<ProfilePage />}/>
+          <Route path='/profile-edit' element={<EditProfile />}/>
+          <Route path='/my-festivals' element={<MyFestivalsPage />}/>
+        </Route>
+
+
+
+
         <Route path='/festivals' element={<FestivalPage />}/>
         <Route path='/artists' element={<ArtistsPage />}/>
-        <Route path='/my-festivals' element={<MyFestivalsPage />}/>
       </Routes>
     </BrowserRouter>
     </>
--- src/components/pagenavigator/PageNavigator.tsx
+++ src/components/pagenavigator/PageNavigator.tsx
@@ -1,17 +1,26 @@
 import { NavLink } from "react-router-dom"
 import './PageNavigator.css'
+import { useAuth } from "../../context/AuthContext"
 
 const PageNavigator: React.FC = () => {
-
+    const { user } = useAuth()
     return (
         <div className="page-navigator">
             <NavLink to={'/'}>Home Page</NavLink>
-            <NavLink to={'/login'}>Login</NavLink>
-            <NavLink to={'/register'}>Register</NavLink>
-            <NavLink to={'/profile'}>My profile</NavLink>
             <NavLink to={'/festivals'}>Festivals</NavLink>
             <NavLink to={'/artists'}>Artists</NavLink>
-            <NavLink to={'/my-festivals'}>My Festivals</NavLink>
+
+        {!user ? (
+            <>
+                <NavLink to={'/login'}>Login</NavLink>
+                <NavLink to={'/register'}>Register</NavLink>
+            </>
+        ) : (
+            <>
+                <NavLink to={'/profile'}>My profile</NavLink>
+                <NavLink to={'/my-festivals'}>My Festivals</NavLink>
+            </>
+        )}
         </div>
     )
 }
